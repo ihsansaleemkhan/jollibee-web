b62b23f8a6db6fc2a24870bec5efa5b456a985c8
saleemkhan <saleemkhan.ihsan@gmail.com>
2018-07-26 17:50:51 +0530

    api

--- app/Http/Controllers/NavigationController.php
+++ app/Http/Controllers/NavigationController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
 
 class NavigationController extends Controller
 {
@@ -16,6 +18,16 @@ class NavigationController extends Controller
    {
        $ProductMaster = new \App\ProductMaster();
        $products = $ProductMaster->getProducts();
+/*
+       $guzzleClient = new Client(['base_uri' => 'http://eportal.mycomsys.com/posapi_json/api/group?cid=70288&lcode=001&from=1-1-2000&to=1-1-2100']);
+       $client = json_api()->client($guzzleClient);
+       */
+       $client = new Client();
+       $res = $client->get('http://eportal.mycomsys.com/posapi_json/api/subgroup?cid=70288&lcode=001&from=1-1-2000&to=1-1-2100');;
+
+       //$client = new Client(); //GuzzleHttp\Client
+      // $res = $client->get('http://eportal.mycomsys.com/posapi_json/api/subgroup?cid=70288&lcode=001&from=1-1-2000&to=1-1-2100');
+       dd($res);
       // dd($products);
        return view('order-online', ['products' => $products]);
    }
