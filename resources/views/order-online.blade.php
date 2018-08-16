5e267f98387e510d3a709f6e0dc6e2fe23edea38
saleemkhan <saleemkhan.ihsan@gmail.com>
2018-08-16 11:23:27 +0530

    location

--- resources/views/order-online.blade.php
+++ resources/views/order-online.blade.php
@@ -75,24 +75,4 @@
         <a href=""><img src="assets/images/chat.png" class="chat"></a>
     </div><!-- .col-full -->
 </div>
-
-<script>
-$('[data-method]').append(function(){
-return "\n"+
-"<form action='"+$(this).attr('href')+"' method='POST' name='delete_item' style='display:none'>\n"+
-"   <input type='hidden' name='_method' value='"+$(this).attr('data-method')+"'>\n"+
-"   <input type='hidden' name='_token' value='"+$('meta[name="_token"]').attr('content')+"'>\n"+
-"</form>\n"
-})
-.removeAttr('href')
-.attr('style','cursor:pointer;')
-.attr('onclick','$(this).find("form").submit();');
-
-/*
-Generic are you sure dialog
-*/
-$('form[name=delete_item]').submit(function(){
-return confirm("Are you sure you want to delete this item?");
-});
-</script>
 @include('partials.footer')
